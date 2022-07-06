bea87badafa808121a5a0f2bb67725647b3b7a15
Piyush <90107289+PiyushKarmhe@users.noreply.github.com>
2022-07-06 17:03:10 +0530

    custom pointer test

--- js/index.js
+++ js/index.js
@@ -6,6 +6,9 @@ const close_menu = ()=>{
 }
 cross.addEventListener('click',close_menu);
 
-const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
+let circle = document.getElementById('circle');
+const onMouseMove = (e) =>{
+  circle.style.left = e.pageX + 'px';
+  circle.style.top = e.pageY + 'px';
 }
+document.addEventListener('mousemove', onMouseMove);
