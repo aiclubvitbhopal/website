55eec59d8c74ca1f21095df2bd9f5bac1c4f751a
Piyush <90107289+PiyushKarmhe@users.noreply.github.com>
2022-07-06 17:11:39 +0530

    highlight feature

--- js/index.js
+++ js/index.js
@@ -7,8 +7,10 @@ const close_menu = ()=>{
 cross.addEventListener('click',close_menu);
 
 let circle = document.getElementById('circle');
+
 const onMouseMove = (e) =>{
   circle.style.left = e.pageX + 'px';
   circle.style.top = e.pageY + 'px';
 }
+
 document.addEventListener('mousemove', onMouseMove);
