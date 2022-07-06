9dc3f98ae3c5776f10f0295cee3645d7c22cfd8c
Piyush <90107289+PiyushKarmhe@users.noreply.github.com>
2022-07-06 18:34:53 +0530

    Custom Mouse Scroll

--- js/index.js
+++ js/index.js
@@ -9,8 +9,18 @@ cross.addEventListener('click',close_menu);
 let circle = document.getElementById('circle');
 
 const onMouseMove = (e) =>{
-  circle.style.left = e.pageX + 'px';
-  circle.style.top = e.pageY + 'px';
+    circle.style.position="absolute";
+    circle.style.left = e.pageX + 'px';
+    circle.style.top = e.pageY + 'px';
 }
 
 document.addEventListener('mousemove', onMouseMove);
+document.addEventListener(
+    'scroll',
+    (event) => {
+        var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+        circle.style.position="fixed";
+        circle.style.top = (document.querySelector('#circle').getBoundingClientRect().top+20) + 'px';
+    }, 
+    { passive: true }
+);
