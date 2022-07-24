6f9c11211ad1a34667e11cc578492ba17d6fa06d
Piyush <90107289+PiyushKarmhe@users.noreply.github.com>
2022-07-24 18:43:41 +0530

    click event added
    Added for all views i.e mobile, tablet, laptop

--- js/people.js
+++ js/people.js
@@ -7,14 +7,22 @@ const web_t = document.querySelectorAll(".web-t")
 const web_btn = document.querySelector(".blog-box.web");
 
 const tech_d = document.querySelectorAll(".tech-d")
-const tech_m = document.querySelectorAll("tech-m")
+const tech_m = document.querySelectorAll(".tech-m")
 const tech_t = document.querySelectorAll(".tech-t")
 const tech_btn = document.querySelector(".blog-box.tech");
 
-// console.log(web_btn);
-// console.log(web_d)
+const event_d = document.querySelectorAll(".event-d")
+const event_m = document.querySelectorAll(".event-m")
+const event_t = document.querySelectorAll(".event-t")
+const event_btn = document.querySelector(".blog-box.event");
 
-const members = [".web-d",".web-m",".web-t"]
+const design_d = document.querySelectorAll(".design-d")
+const design_m = document.querySelectorAll(".design-m")
+const design_t = document.querySelectorAll(".design-t")
+const design_btn = document.querySelector(".blog-box.design");
+
+const members = [".web-d",".web-m",".web-t",".tech-d",".tech-m",".tech-t",".event-d",".event-m",".event-t",".design-d",".design-m",".design-t"];
+
 const close_all = (ele)=>{
     members.forEach(element =>{
         if(element!==ele){
@@ -22,7 +30,6 @@ const close_all = (ele)=>{
             document.querySelectorAll(element).forEach(e =>{e.style.display="none";})
         }
     });
-    console.log("here");
 };
 
 web_btn.addEventListener("click",(e)=>{
@@ -42,25 +49,61 @@ web_btn.addEventListener("click",(e)=>{
         });        
         close_all(".web-d");
     }
-    console.log("hi");
 });
 
 tech_btn.addEventListener("click",(e)=>{
     if(m.matches){
         tech_m.forEach(element => {
             element.style.display="flex";
-            close_all(".tech-m");
         });
+        close_all(".tech-m");
     }else if(t.matches){
         tech_t.forEach(element => {
             element.style.display="flex";
-            close_all(".tech-t");
         });
+        close_all(".tech-t");
     }else{
         tech_d.forEach(element => {
             element.style.display="flex";
-            close_all(".tech-d");
         });        
+        close_all(".tech-d");
     }
-    console.log("hi");
+});
+
+event_btn.addEventListener("click",(e)=>{
+    if(m.matches){
+        event_m.forEach(element => {
+            element.style.display="flex";
+        });
+        close_all(".event-m");
+    }else if(t.matches){
+        event_t.forEach(element => {
+            element.style.display="flex";
+        });
+        close_all(".event-t");
+    }else{
+        event_d.forEach(element => {
+            element.style.display="flex";
+        });     
+        close_all(".event-d");
+    }
+});
+
+design_btn.addEventListener("click",(e)=>{
+    if(m.matches){
+        design_m.forEach(element => {
+            element.style.display="flex";
+        });
+        close_all(".design-m");
+    }else if(t.matches){
+        design_t.forEach(element => {
+            element.style.display="flex";
+        });
+        close_all(".design-t");
+    }else{
+        design_d.forEach(element => {
+            element.style.display="flex";
+        });     
+        close_all(".design-d");
+    }
 });
