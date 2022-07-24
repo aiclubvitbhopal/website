fa3726165f2d569588ec0d454c81ec52c704b076
Piyush <90107289+PiyushKarmhe@users.noreply.github.com>
2022-07-24 21:30:03 +0530

    Update

--- js/about.js
+++ js/about.js
@@ -1,21 +1,21 @@
-let circle = document.getElementById('circle');
+// let circle = document.getElementById('circle');
 
-const onMouseMove = (e) =>{
-    circle.style.position="absolute";
-    circle.style.left = e.pageX + 'px';
-    circle.style.top = e.pageY + 'px';
-}
+// const onMouseMove = (e) =>{
+//     circle.style.position="absolute";
+//     circle.style.left = e.pageX + 'px';
+//     circle.style.top = e.pageY + 'px';
+// }
 
-document.addEventListener('mousemove', onMouseMove);
-document.addEventListener(
-    'scroll',
-    (event) => {
-        var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
-        circle.style.position="fixed";
-        circle.style.top = (document.querySelector('#circle').getBoundingClientRect().top+20) + 'px';
-    }, 
-    { passive: true }
-);
+// document.addEventListener('mousemove', onMouseMove);
+// document.addEventListener(
+//     'scroll',
+//     (event) => {
+//         var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+//         circle.style.position="fixed";
+//         circle.style.top = (document.querySelector('#circle').getBoundingClientRect().top+20) + 'px';
+//     }, 
+//     { passive: true }
+// );
 
 const heroText = document.querySelector(".text");
 const textTop = heroText.getBoundingClientRect().top;
