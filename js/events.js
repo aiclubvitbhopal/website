fb7fb056c20582911d3d3153f6f41f186215bc18
sarfaraj-settyl <sarfaraj.a@settyl.com>
2024-01-29 10:46:55 +0530

    added js based rendering for events, added event details

---
+++ js/events.js
@@ -0,0 +1,268 @@
+const data = {
+  ai4u: {
+    title: "AI 4 U",
+    mode: "Offline",
+    venue: "AB318",
+    date: "",
+    description: "sdasdsa fsadsa fdsfdsfdsf sdfdsfdsf",
+    images: [],
+    thumbnail: "ai4u-min.png",
+  },
+
+  aipoweredmobiledev: {
+    title: "AI Powered Mobile Application development",
+    mode: "Offline",
+    venue: "AB318",
+    date: "",
+    description: "sdasdsa fsadsa fdsfdsfdsf sdfdsfdsf",
+    images: [],
+    thumbnail: "aimobiledevelopment.png",
+  },
+
+  cvweek: {
+    title: "CV Week",
+    mode: "Offline",
+    venue: "AB318",
+    date: "",
+    description: "sdasdsa fsadsa fdsfdsfdsf sdfdsfdsf",
+    images: [],
+    thumbnail: "cv-week.png",
+  },
+  fuzzyclustering: {
+    title: "Collaborative Fuzzy Clustering And it's Application",
+    mode: "Offline",
+    venue: "AB318",
+    date: "",
+    description: "sdasdsa fsadsa fdsfdsfdsf sdfdsfdsf",
+    images: [],
+    thumbnail: "fuzzy-clustering.png",
+  },
+  techyoncore: {
+    title: "Techyon Core",
+    mode: "Offline",
+    venue: "AB318",
+    date: "",
+    description: "sdasdsa fsadsa fdsfdsfdsf sdfdsfdsf",
+    images: [],
+    thumbnail: "techyoncore.jpg",
+  },
+  defeatthecrowd: {
+    title: "Defeat The Crowd",
+    mode: "Offline",
+    venue: "AB318",
+    date: "",
+    description: "sdasdsa fsadsa fdsfdsfdsf sdfdsfdsf",
+    images: [],
+    thumbnail: "defeatthecrowd.jpg",
+  },
+  marvelthelasthunt: {
+    title: "Marvel The Last Hunt",
+    mode: "Offline",
+    venue: "AB318",
+    date: "",
+    description: "sdasdsa fsadsa fdsfdsfdsf sdfdsfdsf",
+    images: [],
+    thumbnail: "marvelthelasthunt.png",
+  },
+  aiconclave: {
+    title: "AI Conclave",
+    mode: "Offline",
+    venue: "AB318",
+    date: "",
+    description: "sdasdsa fsadsa fdsfdsfdsf sdfdsfdsf",
+    images: [],
+    thumbnail: "aiconclove.jpg",
+  },
+  aibreakdown: {
+    title: "AI Breakdown",
+    mode: "Offline",
+    venue: "AB205",
+    date: "9th February'23",
+    description:
+      "This will web a hilarious event of technology and AI bloopers and failures at a Funny presentation competition. It will bring together tech enthusiasts and AI aficionados to share their funniest moments and biggest blunders in the field. Presenters will have a definite time to design and present on a technology or AI-related blooper or failure. A panel of judges will select the winners based on the audience's responses or the overall humour.  In addition to the presentation competition, we will also have a very interactive montage on fixing those errors and a Q&A with the judges. This will be a very great opportunity to let your hair down, relax, and have a good time while learning about the lighter side of technology and AI.",
+    images: ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg"],
+    thumbnail: "ai-breakdown.jpg",
+  },
+  aicrypt: {
+    title: "A.I. Crypt",
+    mode: "Offline",
+    venue: "Auditorium",
+    date: "28th April'23",
+    description:
+      "AICrypt was a one-day event with two sessions - a sit-down interactive dialogue session with Mr. Smith Gonsalves - a renowned Expert in AI-based Cyber Security who has prior entrepreneurial experience with him being the direct of his company ‘CyberSmithSecure’, and an advanced level workshop conducted by Mr Nitin Pandey - Senior Cyber Crime Consultant and coordinator of the Cyberpeace Foundation on  Offensive and defensive security for enterprises using GANs, which will provide a hands on experience on important skills required in the corporate computer science domain to all the participants, who will mostly include 2nd & 3rd year students. It  encouraged students to increase their interest and their keenness to dive into the world of cyber security and artificial intelligence. The interested students had to register through a registration link (hosted on Konfhub - an event hosting website) provided to them through their emails and also from the AI Club’s official Instagram handle. It was an offline event conducted by the AI Club. There was a small entertainment session of about half an hour before the dialogue and workshop as well.",
+    images: [
+      "aicrypt_image1.jpg",
+      "aicrypt_image2.jpg",
+      "aicrypt_image3.jpg",
+      "aicrypt_image4.jpg",
+      "aicrypt_image5.jpg",
+      "aicrypt_image6.jpg",
+      "aicrypt_image7.jpg",
+      "aicrypt_image8.jpg",
+      "aicrypt_image9.jpg",
+      "aicrypt_image10.jpg",
+      "aicrypt_image11.jpg",
+    ],
+    thumbnail: "aicrpyt.png",
+  },
+  chaipecharcha: {
+    title: "C.H.A.I PE CHARCHA",
+    mode: "Offline",
+    venue: "AB515",
+    date: "8th February'23",
+    description:
+      "This event will be for a day of discussions, presentations, and networking at our Artificial Intelligence conference, named “Ch.A.I pe Charcha”. This event will bring together experts from our Academia VIT Bhopal and all other fellow audience to explore the latest developments in every corner of AI, from how to start AI and how to land a job in AI. This event will feature keynote speakers also known as the moderator, a panel discussion on hot topics in the field, and poster sessions showcasing extra skills and implementation of Artificial Intelligence and Machine Learning. Attendees will have the opportunity to hear from moderators and engage in thoughtful discussions about the future of AI. In addition to that, the technical program will also include networking opportunities to connect with fellow attendees and a great chance to learn and get inspired by the latest advances in AI.",
+    images: ["chai_image1.jpg", "chai_image2.jpg", "chai_image3.jpg"],
+    thumbnail: "ch-AI-pe-charcha.jpg",
+  },
+  aiconic: {
+    title: "AIConic",
+    mode: "Offline",
+    venue: "Auditorium",
+    date: "26th July'23",
+    description:
+      "AIConic was a one-day event with two sessions - a sit-down interactive dialogue session with Mr. Rahul Mishra - a renowned Expert in Generative AI and the Head of Growth at Concept Campus in  Bhopal which provided a hands on experience on important skills required in the corporate computer science domain to all the participants, who mostly included 3rd and 4th year students, and a coding based treasure hunt. It encouraged students to increase their interest and their keenness to dive into the world of artificial intelligence.  The interested students had to register through a registration link (google form) provided through their emails and also from the AI Club’s official Instagram handle. It was an offline event conducted by the AI Club. There was a small entertainment session of about half an hour before the dialogue and competition as well.",
+    images: [
+      "aiconic_image1.jpg",
+      "aiconic_image2.jpg",
+      "aiconic_image3.jpg",
+      "aiconic_image4.jpg",
+      "aiconic_image5.jpg",
+      "aiconic_image6.jpg",
+      "aiconic_image7.jpg",
+    ],
+    thumbnail: "aiconic.jpg",
+  },
+  genesis: {
+    title: "Genesis",
+    mode: "Online",
+    venue: "AB318",
+    date: "24th - 25th November'23",
+    description: `Genesis was a two-day virtual event that featured a podcast session and an online workshop. It promised to expand the participants' perspectives, and it covered topics including prompt engineering with LLMs, NLP exploration, and discovering the secrets of successful projects and going abroad for future endeavors. 
+    <br\>
+    <br\>
+    The first day of the workshop, November 24th, 2023, covered the topic of "NLP and LLMs with prompt engineering." The discussion was led by Rishit Chugh and it delved into the fascinating realm of prompt engineering and natural language processing. 
+    <br\>
+    <br\>
+    On November 25, 2023, the second day's events included a podcast about Projects and Going Abroad, with respected alumnus Mr. Kumar Prakhar from our university. These gatherings were thought-provoking and captivating, providing our university's students, particularly those in their pre-final and second year with valuable information and opportunity for networking.
+    `,
+    images: ["genesis_image1.png", "genesis_image2.png", "genesis_image3.png"],
+    thumbnail: "genesis_event.jpg",
+  },
+};
+
+const eventModalId = "event-details-modal";
+const modal = document.getElementById(eventModalId);
+const eventContainer = document.querySelector("#event .flex-img");
+
+const addEvents = (eventId1, eventId2) => {
+  const event1 = eventId1 && data[eventId1];
+
+  const event2 = eventId2 && data[eventId2];
+  const flImage = document.createElement("div");
+  flImage.classList.add("flexbox-img");
+
+  const addCard = (event, id) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    const defaultdescription =
+      "AI Division @ VIT Bhopal presents to you a weekend full of excitement, packed with guidance, gaining insights and many more opportunities";
+
+    card.innerHTML = `
+        <div class="img-container">
+        <img src="./images/event_thumbnails/${event.thumbnail}" />
+        <div class="overlay">
+          <div class="econtent">
+          <h2>${event.title}</h2>
+          <br/>
+            <h5>
+              <i class="fa fa-calendar"></i> ${event.date}
+            </h5>
+            <h5 class="event-mode">
+              <i class="fa fa-globe"></i> ${event.mode}
+            </h5>
+            <p>
+              ${event.description.substring(0, 100) || defaultdescription}...
+            </p>
+            <div class="button-parent">
+            <div data-event-id="${id}" class="event-open-button">view details<div/>
+            <div/>
+          </div>
+        </div>
+      </div>
+        
+        `;
+
+    flImage.appendChild(card);
+  };
+
+  event1 && addCard(event1, eventId1);
+  event2 && addCard(event2, eventId2);
+
+  eventContainer.appendChild(flImage);
+};
+
+const renderEvents = () => {
+  let pair = [];
+  const eventIds = Object.keys(data).reverse();
+  eventIds.forEach((eventId, index) => {
+    pair.push(eventId);
+    if (index % 2 == 1 || index === eventIds.length - 1) {
+      const [key1, key2] = pair;
+
+      addEvents(key1, key2);
+
+      pair = [];
+    }
+  });
+
+  const scrollEnd = document.createElement("div");
+  scrollEnd.className = "desc-text";
+  scrollEnd.innerHTML = "<p>* Scroll Here for More! *</p>";
+
+  eventContainer.appendChild(scrollEnd);
+};
+
+const setModalHandlers = () => {
+  const openButtons = document.querySelectorAll(".event-open-button");
+
+  const title = document.querySelector("#event-details-modal #title");
+  const date = document.querySelector("#event-details-modal #date span");
+  const poster = document.querySelector(
+    "#event-details-modal .poster-container img"
+  );
+  const description = document.querySelector(
+    "#event-details-modal #description"
+  );
+  const venue = document.querySelector("#event-details-modal #venue span");
+  const mode = document.querySelector("#event-details-modal #mode");
+  const imageSection = document.querySelector(
+    "#event-details-modal .image-section"
+  );
+
+  openButtons.forEach((button) => {
+    const eventId = button.getAttribute("data-event-id");
+    button.addEventListener("click", () => {
+      const eventData = data[eventId];
+
+      title.innerHTML = eventData.title;
+      venue.innerHTML = eventData.venue;
+      date.innerHTML = eventData.date;
+      mode.innerHTML = eventData.mode;
+      description.innerHTML = eventData.description;
+      poster.src = `./images/event_thumbnails/${eventData.thumbnail}`;
+      imageSection.innerHTML = "";
+
+      eventData.images.forEach((src) => {
+        const img = document.createElement("img");
+        img.src = `./images/eventimages/${eventId}/${src}`;
+
+        imageSection.appendChild(img);
+      });
+      modal.classList.add("active");
+    });
+  });
+};
+
+renderEvents();
+setModalHandlers();
